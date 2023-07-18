e40dc804487a6fdcb932252916405339a8d96840
SoliBlack <SolitaireBlack21@gmail.com>
2023-07-17 23:02:46 -0400

    Seems I've completed most of this
    I wish there was more time in the day. I was able to get a lot of the
    objectives completed while making a rock, paper, scissors game. I've
    created multiple functions that link with one another. I'm still going
    to mess with them a bit more and see what parts of the function I can
    break down further and understand better. Practice makes perfect.

---
+++ javas.js
@@ -0,0 +1,60 @@
+let playerWinCount = 0;
+let computerWinCount = 0;
+let tieCount = 0;
+let playerSelects = "";
+let computerSelects = "";
+
+
+//This function randomly returns either ‘Rock’, ‘Paper’ or ‘Scissors’.
+function getComputerChoice(){
+    computerSelects = ["rock", "paper", "scissors"] [Math.floor(Math.random() * 3)];
+    return computerSelects;
+}
+
+//This function plays a single round of Rock Paper Scissors. The function should take two parameters:
+//the playerSelection and computerSelection - and then return a string that declares the winner of the round.
+function playRound(playerSelection, computerSelection) {
+
+    
+    console.log(`You chose ${playerSelection}.`)
+    console.log(`The computer chose ${computerSelection}.`)
+
+    if (playerSelection === computerSelection) {
+        tieCount++;
+        return (`It's a tie! Score is Player: ${playerWinCount} | Computer: ${computerWinCount} | Tie: ${tieCount}`);
+    }
+    //Player wins
+    else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "rock")) {
+        playerWinCount++;
+        return (`You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}! Score is Player: ${playerWinCount} | Computer: ${computerWinCount} | Tie: ${tieCount}`);
+        
+    }
+
+    //Player loses
+    else if ((computerSelection === "rock" && playerSelection === "scissors") || (computerSelection === "scissors" && playerSelection === "paper") || (computerSelection === "paper" && playerSelection === "rock")) {
+        computerWinCount++;
+        //console.log(`Player: ${playerWinCount}\nComputer: ${computerWinCount}\nTie: ${tieCount}`);
+        return (`You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}! Score is Player: ${playerWinCount} | Computer: ${computerWinCount} | Tie: ${tieCount}`);
+        
+    }
+    else {
+        return ("Someting went wrong, try again!");
+    }
+}
+
+function game(){
+
+    for (let i = 0; i < 5; i++){
+        getplayerChoice();
+        let result = playRound(playerSelects, getComputerChoice());
+        console.log(result);
+        
+    }
+    
+
+}
+
+function getplayerChoice(){
+    playerSelects = prompt("Rock, Paper, Scissors?: ").toLowerCase();
+    return playerSelects;
+}
