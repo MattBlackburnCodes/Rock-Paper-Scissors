a164cec8d58a0241665f94bab0757a7e63dddc59
SoliBlack <SolitaireBlack21@gmail.com>
2023-08-13 12:23:56 -0400

    Finished the core part of the game
    I have finished the core part of the game. The game is playable now.
    Now it's time to really make the UI better and cooler looking. I want
    to add more picture and change the color scheme. I will work on the
    layout and design but that will be a litle bit later. Now it is time to
    rest.

--- javas.js
+++ javas.js
@@ -4,11 +4,26 @@ let tieCount = 0;
 let playerSelects = "";
 let computerSelects = "";
 
+const paper = document.querySelector('.paper');
+const rock = document.querySelector('.rock');
+const scissors = document.querySelector('.scissors');
+const playerOutput = document.querySelector('.playerOutput');
+const compOutput = document.querySelector('.compOutput');
+const gameOutcome = document.querySelector('.gameOutcome');
+
+const playerScore = document.querySelector('.playerScore');
+const compScore = document.querySelector('.compScore');
+const tieScore = document.querySelector('.tieScore');
+const gameResult = document.querySelector('.gameResult');
 
 function resetGame(){
     playerWinCount = 0;
+    playerScore.textContent = playerWinCount;
     computerWinCount = 0;
+    compScore.textContent = computerWinCount;
     tieCount = 0;
+    tieScore.textContent = tieCount;
+    
     playerSelects = "";
     computerSelects = "";
 }
@@ -16,15 +31,45 @@ function resetGame(){
 //This function randomly returns either ‘Rock’, ‘Paper’ or ‘Scissors’.
 function getComputerChoice(){
     computerSelects = ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
+    compOutput.textContent = `The computer chose ${computerSelects}.`;
     console.log(`The computer chose ${computerSelects}.`)
     return computerSelects;
 }
 
-function getplayerChoice(){
-    playerSelects = prompt("Rock, Paper, Scissors?: ").toLowerCase();
-    console.log(`You chose ${playerSelects}.`)
-    return (`You choose ${playerSelects}`);
+
+function getPlayerChoice(choice){
+    //const playerOutput = document.querySelector('.playerOutput');
+        
+    playerSelects = choice;
+    playerOutput.textContent = `You chose ${playerSelects}.`;
+    return playerSelects;
+
 }
+        
+// Below are a list of event listeners for the options
+paper.addEventListener('click', () => {
+    getPlayerChoice("paper");
+    getComputerChoice();
+    playRound(playerSelects, computerSelects);
+    gameResults();
+})
+
+rock.addEventListener('click', () => {
+    console.log("Clicked");
+    getPlayerChoice("rock");
+    getComputerChoice();
+    playRound(playerSelects, computerSelects);
+    gameResults();
+})
+
+scissors.addEventListener('click', () => {
+    console.log("Clicked");
+    getPlayerChoice("scissors");
+    getComputerChoice();
+    playRound(playerSelects, computerSelects);
+    gameResults();
+})
+
 
 
 //This function plays a single round of Rock Paper Scissors. The function should take two parameters:
@@ -35,11 +80,15 @@ function playRound(playerSelection, computerSelection) {
     //Tie
     if (playerSelection === computerSelection) {
         tieCount++;
-        return (`It's a tie! Score is Player: ${playerWinCount} | Computer: ${computerWinCount} | Tie: ${tieCount}`);
+        tieScore.textContent = tieCount;
+        gameOutcome.textContent = (`It's a tie!`);
+        return (`It's a tie!`);
     }
     //Player wins
     else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "paper") || (playerSelection === "paper" && computerSelection === "rock")) {
         playerWinCount++;
+        playerScore.textContent = playerWinCount;
+        gameOutcome.textContent = (`You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}!`);
         return (`You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}! Score is Player: ${playerWinCount} | Computer: ${computerWinCount} | Tie: ${tieCount}`);
         
     }
@@ -47,20 +96,41 @@ function playRound(playerSelection, computerSelection) {
     //Player loses
     else if ((computerSelection === "rock" && playerSelection === "scissors") || (computerSelection === "scissors" && playerSelection === "paper") || (computerSelection === "paper" && playerSelection === "rock")) {
         computerWinCount++;
+        compScore.textContent = computerWinCount;
+        gameOutcome.textContent = (`You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}!`);
+        
         return (`You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}! Score is Player: ${playerWinCount} | Computer: ${computerWinCount} | Tie: ${tieCount}`);
         
     }
     else {
         return ("Someting went wrong, try again!");
     }
+    
+
+    
 }
 
-function game(){
+function gameResults(){
+    if (playerWinCount === 5){
+        gameResult.textContent = ("You won the game! You are the champion of the world!");
+        resetGame();
+    }
+    else if (computerWinCount === 5){
+        gameResult.textContent = ("You have lost the game to the computer! All hope for humanity is lost!");
+        resetGame();
+    }
+    else {
+        gameResult.textContent = ("");
+    }
+    
+}
+
+/*function game(){
 
     //Loops the game 5 times
     for (let i = 0; i < 5; i++){
         //Get the player and computer choices
-        getplayerChoice();
+        //getPlayerChoice();
         getComputerChoice();
 
         //Plays a single round
@@ -71,7 +141,7 @@ function game(){
     resetGame();
     
 
-}
+}*/
 
 
 
